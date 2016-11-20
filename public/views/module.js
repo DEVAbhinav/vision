b93736a79d5c5b033b668e60033d555fa43cc79e
adi <adityaa803@gmail.com>
2016-11-20 10:46:57 +0530

    Angular happened

--- public/views/module.js
+++ public/views/module.js
@@ -1,17 +1,17 @@
-angular.module('startMean', ['ngRoute', 'nerd']).config(function($routeProvider) {
+angular.module('startMean', ['ngRoute']).config(function($routeProvider) {
     // route for the home page
     $routeProvider
         .when('/', {
-            templateUrl: '/views/login.html'
-                // controller: 'mainCtrl'
+            templateUrl: '/views/login.html',
+            controller: 'loginCtrl'
         })
         .when('/view', {
-            templateUrl: '/views/filter.html'
-                // controller: 'mainCtrl'
+            templateUrl: '/views/filter.html',
+            controller: 'viewCtrl'
         })
         .when('/profile', {
-            templateUrl: '/views/details.html'
-                // controller: 'mainCtrl'
+            templateUrl: '/views/details.html',
+            controller: 'profileCtrl'
         })
         .otherwise({
             redirectTo: '/'
